5606e7d337fac0e38579f755dcf99a8ee999a6c2
EvoX7 <creativesoultouch@gmail.com>
2022-07-04 18:37:24 +0200

    Image added - bootstraps html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const app = new Vue({
-    el: '#root',
+    el: "#root",
     data: {
-        message: 'Hello i am Vue, your new front-end developer tool!',
+      message: "Hello i am Vue, your new front-end developer tool!",
+      imgUrl:
+        "https://images.unsplash.com/photo-1656867722247-ec21deee1bfb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
     }
-    });
+  });
+  
